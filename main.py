64ba8b0194ece8407c352a8795a6f53406a3bd5b
JossyrUpM <61509603+JossyrUpM@users.noreply.github.com>
2020-04-11 18:15:56 -0500

    Primera modificación si se ingresa 0 (a_power_b)

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import numpy as np
 a = int(input("Digite la base: "))
 b = int(input("Digite el exponente: "))
 def a_power_b(a,b):
-    contP= 1
+     contP= 1
+     print("Si la base digitada es 0, se detendrá la función")
+while a != (0): 
     for i in range(0,b,) :
         contP = contP * a
-    print(contP)
+        print(contP)
 
 a_power_b(a,b)
