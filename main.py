34fe43be30c66f89b27f5eea77bd74dd75d1ac63
JossyrUpM <61509603+JossyrUpM@users.noreply.github.com>
2020-04-11 19:11:12 -0500

    Correción de errores a_power_b al ingresar 0

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import numpy as np
 a = int(input("Digite la base: "))
 b = int(input("Digite el exponente: "))
 def a_power_b(a,b):
-     contP= 1
-     print("Si la base digitada es 0, se detendrá la función")
-while a != (0): 
-    for i in range(0,b,) :
-        contP = contP * a
-        print(contP)
+    contP= 1
+    while a!=0:
 
+        for i in range(0,b,) :
+           contP = contP * a
+        print(contP)
+        a = int(input("Digite la base: "))
+        b = int(input("Digite el exponente: "))
 a_power_b(a,b)
