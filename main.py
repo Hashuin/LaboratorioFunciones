6760a9e3f021be554eaf0276c0b3525fd4bf296e
JossyrUpM <61509603+JossyrUpM@users.noreply.github.com>
2020-04-11 19:22:56 -0500

    Modificación en la que se agregaron los contadores de pares e impares

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ a = int(input("Digite la base: "))
 b = int(input("Digite el exponente: "))
 def a_power_b(a,b):
     contP= 1
+    CPar= 0
+    CImpar = 0
     while a!=0:
 
         for i in range(0,b,) :
            contP = contP * a
+           if contP % 2 == 0 :
+            CPar=CPar + 1
+           elif contP % 2 != 0:
+             CImpar=CImpar + 1
         print(contP)
+        print("Veces par",CPar)
+        print("Veces impar",CImpar)
         a = int(input("Digite la base: "))
         b = int(input("Digite el exponente: "))
 a_power_b(a,b)
